7b46c6c138054f757457184b546d632ebeef2aa9
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-03-10 17:08:57 +0200

    Adding js files to be excuted in browser

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+/* Adding javascript in the head tag will be prevented
+ the browser to be parsed the whole html doc.
+so, added in the end of body tag after loading, parsing the html file
+then js files loaded
+*/
+
+alert('This works!');
