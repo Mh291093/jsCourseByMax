676779e9398668cece352e531e2e08f41b5eeae2
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-03-11 20:56:24 +0200

    video 18,19,20,21

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,5 @@
-/* Adding javascript in the head tag will be prevented
- the browser to be parsed the whole html doc.
-so, added in the end of body tag after loading, parsing the html file
-then js files loaded
-*/
+let currentResult = 0;
 
-alert('This works!');
+currentResult = (currentResult + 10) * 3 / 2 -1;
+
+outputResult(currentResult, "");
---
+++ assets/scripts/notes.js
@@ -0,0 +1,45 @@
+// Video No. = 18 "Adding JavaScript to the website"
+/* Adding javascript in the head tag will be prevented
+ the browser to be parsed the whole html doc.
+so, added in the end of body tag after loading, parsing the html file
+then js files loaded
+*/
+
+// Video No. = 19 "Introducing Variables & Constants"
+/* variables, Constants are a data container or data storage
+keyWords (let, var)
+you can change value without using keyword let or var
+ex: let userName = "Mohamed";
+userName = "Ahmed";
+keyWord (const)
+you can't change value 
+ex: const age = 29;
+age = 30; this is yields an error
+*/
+
+// Video No. = 20 "Declaring & Defining Variables"
+/*
+Variable Names Rules 
+Allowed : 
+1. camelCase this is the best practice                       ex: let userName
+2. snake_case this is allowed but bad practice               ex: let user_name
+3. starting with $ and _                                     ex: let _user or let $name
+4. letters and digits in the end or the middle of variable   ex: let userName7
+notAllowed :
+1. starting with letters or digits                           ex: let 1userName
+2. no special characters such as (-,@,&)                     ex: let user-name
+3. key words such as (let,if,for)                            ex: let let
+Declaring a variable without initializing                    ex: let userName
+Set a value to a variable by adding assignment operator =    ex: userName = "Mo";
+*/
+
+// Video No. = 21 "Working with Variables & Operators"
+/*
+opertors are a core syntax in javascript which manipulates values
+ex: Addition + 
+    Subtraction -
+    Multiplication *
+    Division /
+    Modulus %    yields remainder   ex: 7%4 = 3
+    Exponentiation **               ex: 2**3 = 8
+*/
